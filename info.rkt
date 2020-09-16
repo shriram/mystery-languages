86f717654c9ff05b7f838ea7ba1e63fb280d9387
Shriram Krishnamurthi <sk@cs.brown.edu>
2020-09-16 00:04:59 -0400

    updated info

--- info.rkt
+++ info.rkt
@@ -1,4 +1,4 @@
 #lang info
 (define collection "mystery-languages")
-(define version "2020.1")
-(define deps (list "exact-decimal-lang" "https://github.com/shriram/smol.git"))
+(define version "2020.2")
+(define deps (list "lazy" "exact-decimal-lang" "https://github.com/shriram/smol.git"))
